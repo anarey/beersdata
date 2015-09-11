50fc56494be42119981fe620f1e4d085cb494c2c
Ana Rey <anarey@gmail.com>
2015-09-11 12:13:19 +0200

    Beers: vistas y template para mostrar cervezas e ingredientes
    Creada vistas y template que hacen:
    
    * Listado de cervezas disponibles.
    * Listado de ingredientes disponibles.
    * Información sobre una cerveza específica.
    * Información sobre un ingrediente.

--- beers/urls.py
+++ beers/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'),
     url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail-beers'),
-    url(r'^/ingredient/(?P<pk>[0-9]+)/$', views.IngredientView.as_view(), name='details-ingredients'),
+    url(r'^ingredient/$', views.IndexViewIngredient.as_view(), name='index-ingredients'),
+    url(r'^ingredient/(?P<pk>[0-9]+)/$', views.IngredientView.as_view(), name='detail-ingredients'),
 ]
--- beers/views.py
+++ beers/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.views import generic
+
+from .models import Beer, Ingredient
+
+class IndexView(generic.ListView):
+    template_name = 'beers/index.html'
+    context_object_name = 'beers_list'
+
+    def get_queryset(self):
+        """Return the last five published beers."""
+        return Beer.objects.order_by('-brewing_date')
+
+class DetailView(generic.DetailView):
+    model = Beer
+    template_name = 'beers/detail.html'
+
+class IngredientView(generic.DetailView):
+    model = Ingredient
+    template_name = 'beers/ingredient-detail.html'
+
+class IndexViewIngredient(generic.ListView):
+    template_name = 'beers/index-ingredient.html'
+    context_object_name = 'ingredient_list'
+
+    def get_queryset(self):
+        """Return all Ingredient."""
+        return Ingredient.objects.order_by('-type_ingredient').order_by('name')
