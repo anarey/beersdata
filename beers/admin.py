c6d53a55e44be5682c6f981f7dba1cadeb1892a3
Ana Rey <anarey@gmail.com>
2015-09-23 11:45:04 +0200

    admin: corregido error al importar models

--- beers/admin.py
+++ beers/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from models import Recipe, Ingredient, Beer, Purchase
+from .models import Recipe, Ingredient, Beer, Purchase
 
 class RecipeInLine(admin.TabularInline):
     model = Recipe
